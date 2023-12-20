34da205f37d5abefe6e688d8223bff1177cb3e46
Edwin Alberto Aldana Ricaurte <edwin.aldana@adres.gov.co>
2023-12-19 23:38:27 -0500

    Gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8001/api/',
-  backend: 'http://localhost:8001/'
+  apiUrl: 'http://127.0.0.1:8000/api/',
+  backend: 'http://localhost:8000/'
 };
 
 /*
